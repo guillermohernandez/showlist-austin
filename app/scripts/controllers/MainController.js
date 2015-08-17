82dd3e73a7698ed6134c57d6b26df604930cfc45
Guillermo Hernandez <guillermohernandez@gmail.com>
2015-08-17 15:03:24 -0500

    Initial dist subtree commit

---
+++ app/scripts/controllers/MainController.js
@@ -0,0 +1,34 @@
+app.controller('MainController', ['$scope', function ($scope) {
+    $scope.shows = [
+        {
+            lineup: 'Royal Headache, Bad Sports, Jonly Bonly',
+            venue: 'Red 7',
+            time: '8 PM',
+            cover: 5.00
+   },
+        {
+            lineup: 'Speedy Sparks and the Koolerators',
+            venue: 'Hotel Vegas',
+            time: '9 PM',
+            cover: 5.00
+   },
+        {
+            lineup: 'Rock N\' Roll Karaoke',
+            venue: 'Beerland',
+            time: '9 PM',
+            cover: 5.00
+   },
+        {
+            lineup: 'Katie Butcher, The Love Sprockets, Ravi Nulu & Micah Wagner',
+            venue: 'Scoot Inn',
+            time: '8 PM',
+            cover: 5.00
+   },
+        {
+            lineup: 'Roger Wallace, Lindsey Verrill, Sweet Gary Newcomb',
+            venue: 'Hole in the Wall',
+            time: '9 PM',
+            cover: 5.00
+   }
+  ];
+}]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name showlistAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the showlistAppApp
+ */
+angular.module('showlistAppApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
